076e4473fbcfa2c70b09535801b6f7f3911dba5a
javierddiaz <javier.diaz@cvudes.edu.co>
2022-06-16 13:46:49 -0500

    sena

--- config/db.php
+++ config/db.php
@@ -4,13 +4,13 @@
 	Web: obedalvarado.pw
 	Mail: info@obedalvarado.pw
 	---------------------------*/
-/*	
+	
 define("DB_HOST", "yjo6uubt3u5c16az.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");//DB_HOST:  generalmente suele ser "127.0.0.1"
 define("DB_NAME", "l0c9ffa2g4g2f3ck");//Nombre de la base de datos
 define("DB_USER", "v9inrp1duhau6tit");//Usuario de tu base de datos
 define("DB_PASS", "qc6o2oklk0a5mgzx");//Contraseña del usuario de la base de datos
-*/
+/*
 define("DB_HOST", "localhost");//DB_HOST:  generalmente suele ser "127.0.0.1"
-define("DB_NAME", "cotizador");//Nombre de la base de datos
+define("DB_NAME", "cotizador_bs3");//Nombre de la base de datos
 define("DB_USER", "root");//Usuario de tu base de datos
-define("DB_PASS", "");
+define("DB_PASS", "");*/
