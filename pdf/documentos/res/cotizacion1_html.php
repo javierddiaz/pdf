35da842bffcf535d7a8c7d34677b9f0587650101
javierddiaz <javier.diaz@cvudes.edu.co>
2022-06-16 13:45:44 -0500

    sena

--- pdf/documentos/res/cotizacion1_html.php
+++ pdf/documentos/res/cotizacion1_html.php
@@ -61,40 +61,33 @@
         </tr>
     </table>
     <?php
-    exit;
     $sumador_total = 0;
-    $sql= mysqli_query($con, "select max(numero_cotizacion) FROM cotizaciones_demo WHERE empresa=" . $_SESSION['codigoU']);
+    $sql= mysqli_query($con, "select max(numero_cotizacion) FROM cotizaciones_demo WHERE empresa='" . $_SESSION['codigoU']."'");
     $row = mysqli_fetch_array($sql);
-    echo $row[0];
-    exit;
+    $solicitud=$row[0];
     $sql = mysqli_query($con, "select * FROM detalle_cotizacion_demo WHERE numero_cotizacion=" . $solicitud);
-    $sql = mysqli_query($con, "select * from productos_demo, detalle_cotizacion_demo where productos_demo.id_producto=tmp_cotizacion.id_producto and tmp_cotizacion.session_id='" . $session_id . "'");
     while ($row = mysqli_fetch_array($sql)) {
-        $id_tmp = $row["id_tmp"];
-        $id_producto = $row["id_producto"];
-        $codigo_producto = $row['codigo_producto'];
-        $cantidad = $row['cantidad_tmp'];
-        $nombre_producto = $row['nombre_producto'];
-        $precio_venta = $row['precio_tmp'];
-        $precio_venta_f = number_format($precio_venta, 2); //Formateo variables
-        $precio_venta_r = str_replace(",", "", $precio_venta_f); //Reemplazo las comas
-        $precio_total = $precio_venta_r * $cantidad;
-        $precio_total_f = number_format($precio_total, 2); //Precio total formateado
-        $precio_total_r = str_replace(",", "", $precio_total_f); //Reemplazo las comas
-        $sumador_total += $precio_total_r; //Sumador
+        $cantidad=$row['cantidad'];
+        $precioU=$row['precio_venta'];
+        $producto=$row['id_producto'];
+        $sql1 = mysqli_query($con, "select * FROM productos_demo WHERE id_producto=" . $producto);
+	    $row1 = mysqli_fetch_array($sql1);
+        $producto=$row1['nombre_producto'];
+        $total=$cantidad*$precioU;
+        $sumador_total += $total;
     ?>
         <table cellspacing="0" style="width: 100%; border: solid 1px black;  text-align: center; font-size: 11pt;padding:1mm;">
             <tr>
                 <td style="width: 10%; text-align: center"><?php echo $cantidad; ?></td>
-                <td style="width: 60%; text-align: left"><? echo $nombre_producto ?></td>
-                <td style="width: 15%; text-align: right"><? echo $precio_venta_f; ?></td>
-                <td style="width: 15%; text-align: right"><? echo $precio_total_f; ?></td>
+                <td style="width: 60%; text-align: left"><? echo $producto ?></td>
+                <td style="width: 15%; text-align: right"><? echo number_format($precioU,2); ?></td>
+                <td style="width: 15%; text-align: right"><? echo number_format($total,2); ?></td>
 
             </tr>
         </table>
     <?php
         //Insert en la tabla detalle_cotizacion
-       $insert_detail = mysqli_query($con, "INSERT INTO detalle_cotizacion_demo (numero_cotizacion,id_producto,cantidad,precio_venta) VALUES ('$numero_cotizacion','$id_producto','$cantidad','$precio_venta_r')");
+      // $insert_detail = mysqli_query($con, "INSERT INTO detalle_cotizacion_demo (numero_cotizacion,id_producto,cantidad,precio_venta) VALUES ('$numero_cotizacion','$id_producto','$cantidad','$precio_venta_r')");
 
     }
 
@@ -120,8 +113,8 @@ $date = date("Y-m-d H:i:s");
 $sql = mysqli_query($con, "SELECT sum(`cantidad_tmp`*`precio_tmp`) FROM `tmp_cotizacion` WHERE `session_id`='" . $_SESSION['codigoU'] . "'");
 $row = mysqli_fetch_array($sql);
 $saldo=$_SESSION['techo'] -$row[0];
-$insert = mysqli_query($con, "INSERT INTO cotizaciones_demo VALUES (null,'$numero_cotizacion','$date','$area','$tel1','$session_id','','$email','$saldo','$validez','$entrega')");
-mysqli_query($con, "UPDATE tb_user set useremail=".$saldo." WHERE id=" . $_SESSION['codigoU'] );
-$delete=mysqli_query($con,"DELETE FROM tmp_cotizacion WHERE session_id='".$_SESSION['codigoU']."'");
+//$insert = mysqli_query($con, "INSERT INTO cotizaciones_demo VALUES (null,'$numero_cotizacion','$date','$area','$tel1','$session_id','','$email','$saldo','$validez','$entrega')");
+//mysqli_query($con, "UPDATE tb_user set useremail=".$saldo." WHERE id=" . $_SESSION['codigoU'] );
+//$delete=mysqli_query($con,"DELETE FROM tmp_cotizacion WHERE session_id='".$_SESSION['codigoU']."'");
 
 ?>
